b74cbf618bb9a9e52fff64fd0ebae58b37e673e8
narsing17 <narsingp14@gmail.com>
2022-05-28 15:17:53 +0530

    added Inode

---
+++ INode.java
@@ -0,0 +1,12 @@
+package stack.com;
+
+public class INode {
+    int data;
+    INode next;
+
+    // Parameter Constructor
+    INode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
